47c2b395004e634a0d53ae96391dff49e410d0de
Dmitry Ponyatov <dponyatov@gmail.com>
2024-06-12 17:26:40 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/meta.fs
+++ lib/meta.fs
@@ -11,8 +11,19 @@ let dir =
 dir
 
 let giti =
+    let mask =
+        Map["", [ "*~"; "*.swp"; "*.log"; ""; "/docs/"; "/obj/"; "/meta/"; "" ]
+            "/.vscode", []
+            "/bin", [ "*" ]
+            "/doc", [ "" ]
+            "/inc", [ "" ]
+            "/lib", [ "" ]
+            "/src", [ "" ]
+            "/tmp", [ "*" ]
+            "/ref", [ "*" ]]
+
     for i in dirs do
-        File.WriteAllLines("meta" + i + "/.gitignore", [ "!.gitignore" ])
+        File.WriteAllLines("meta" + i + "/.gitignore", mask[i] @ [ "!.gitignore" ])
 
 giti
 
